3690158a2bac76966d626a068de70da0063c17af
Jennifer Mckay <jen.y.loaa@gmail.com>
2020-05-12 21:01:43 -0600

    commit 1

--- src/App.js
+++ src/App.js
@@ -1,26 +1,52 @@
 import React from 'react';
-import logo from './logo.svg';
+import ReactDOM from 'react-dom'
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class DrumMachine extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    }
+    this.onKey = this.onKey.bind(this)
+  }
+  onKey(event) {
+    this.setState({
+
+    });
+  }
+  render() {
+    return (
+      <div className="container-fluid">
+        <div id="drum-machine">
+          <div className="row">
+          <div id="display"></div>
+          </div>
+          <div className="row">
+            <div className="drum-pad col-xs-3" id="Q">X</div>
+            <div className="drum-pad col-xs-3" id="W">x</div>
+            <div className="drum-pad col-xs-3" id="E">x</div>
+          </div>
+          <div className="row">
+            <div className="drum-pad col-xs-3" id="A">x</div>
+            <div className="drum-pad col-xs-3" id="S">x</div>
+            <div className="drum-pad col-xs-3" id="D">x</div>
+          </div>
+          <div className="row">
+            <div className="drum-pad col-xs-3" id="Z">x</div>
+            <div className="drum-pad col-xs-3" id="X">x</div>
+            <div className="drum-pad col-xs-3" id="C">x</div>
+          </div>
+        </div>
+
+
+
+      </div>
+    )
+  }
 }
 
-export default App;
+ReactDOM.render(<DrumMachine />, document.getElementById('root'))
+
+export default DrumMachine;
